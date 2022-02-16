405c73e0d4ad55d0524f60262ecd8ba23e6adf82
george <jogos_townsend@hotmail.com>
2022-02-15 21:52:42 -0300

    ATT

---
+++ backend/src/router.ts
@@ -0,0 +1,26 @@
+import { SelectUserController } from './controllers/SeletUserController';
+import { UpdateUserController } from './controllers/UpdateUserController';
+import { DeleteUserController } from './controllers/DeleteUserController';
+import { CreateUserController } from './controllers/CreateUserController';
+import { Router } from "express";
+
+
+
+
+const router = Router();
+
+
+const CreateUser = new CreateUserController()
+const DeleteUser = new DeleteUserController()
+const UpdateUser = new UpdateUserController()
+const SelectUser = new SelectUserController()
+
+
+router.get("/all", SelectUser.handle)
+router.put ("/update", UpdateUser.handle)
+router.post("/user", CreateUser.handle)
+router.delete("/userdel", DeleteUser.handle)
+
+
+
+export { router };
